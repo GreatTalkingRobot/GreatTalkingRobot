1315623fc7e528a43d2bbc5a09b7f9d7c8af2093
Qian <qianfeng@gmail.com>
2012-12-05 17:30:38 -0500

    add

---
+++ GreatTalkingRobot/src/edu/towson/cs/greattalkingrobot/shared/ConsistantValues.java
@@ -0,0 +1,7 @@
+package edu.towson.cs.greattalkingrobot.shared;
+
+public class ConsistantValues {
+	public final static String ROBOT_NAME="Matilda";
+	public final static String ROBOT_URL="http://www.pandorabots.com/pandora/talk?botid=c340d2ec6e348c5f";
+
+}
